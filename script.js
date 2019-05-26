b4dc10e0b82bab2ff7426635a14996eb12d165eb
shashi <shashir867@gmail.com>
2019-05-26 19:14:27 -0400

    changes in script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
+//assigning constant values
 const colorBtn = document.querySelector('.colorBtn');
+
 const bodyBcg = document.querySelector('body');
-console.log('bodyBcg',bodyBcg)
 
 const colors = ['yellow','red','green','#3b5785'];
 
+//Listening to click event
 colorBtn.addEventListener('click',changeColor);
 
+//changing color when we click
  function changeColor(){
+     
      let random = Math.floor(Math.random()*colors.length)
-     console.log('Math.random',Math.random())
-     console.log('colors.length',colors.length)
-     console.log('random',random) 
-     console.log('colors',colors[random])
-
-
+     
      bodyBcg.style.backgroundColor = colors[random];
      
        
